5a24d088db2cffd1b7d5767540002db6e6c48919
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 15:55:26 -0300

    Segundo Exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exemplos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exemplos.js
@@ -1,5 +1,12 @@
+// Exercicio 1 [Array]
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
 
+// Exercicio 2 [Array]
+
+let menuDois = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio =  menuDois.indexOf('Portfólio');
+console.log(indexOfPortfolio);
