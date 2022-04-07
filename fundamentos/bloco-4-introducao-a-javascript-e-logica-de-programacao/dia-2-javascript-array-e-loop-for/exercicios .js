95800f96ec5f72a5d82a1004759656820f1a82b8
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 19:19:07 -0300

    Exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
@@ -61,20 +61,32 @@
 
 // Exercicio 6
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numerosImpares = 0;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numerosImpares = 0;
 
-for(let index = 0; index < numbers.length; index += 1) {
-  if(numbers[index] % 2 !== 0) {
-    numerosImpares += 1;
+// for(let index = 0; index < numbers.length; index += 1) {
+//   if(numbers[index] % 2 !== 0) {
+//     numerosImpares += 1;
+//   }
+// };
+
+// if(numerosImpares !== 0) {
+//   console.log(numerosImpares)
+// } else {
+//   console.log("Nenhum valor ímpar encontrado")
+// };
+
+//Exercicio 7
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroMenor = numbers[0];
+for(let index = 1; index < numbers.length; index += 1) {
+  if(numbers[index] < numeroMenor) {
+    numeroMenor = numbers[index]
   }
 };
 
-if(numerosImpares !== 0) {
-  console.log(numerosImpares)
-} else {
-  console.log("Nenhum valor ímpar encontrado")
-};
+console.log(numeroMenor);
 
 
 
