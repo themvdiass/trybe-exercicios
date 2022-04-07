93db690fb1db8e22bc71805959ce60744701a38e
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 18:32:26 -0300

    Exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
@@ -11,4 +11,18 @@ console.log(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + num
 // Exercicio 3
 
 let contagem = numbers.length;
-console.log((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem)
+console.log((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem);
+
+// Exercicio 4
+
+let total = [];
+total.push((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem);
+if(total > 20) {
+  console.log("Valor maior que 20")
+} else {
+  console.log("Valor menor que 20")
+};
+
+
+
+
