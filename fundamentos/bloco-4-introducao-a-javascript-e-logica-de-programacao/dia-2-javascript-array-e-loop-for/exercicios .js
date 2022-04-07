651d606be36c8f79e68e9db8b19bb4800b5f35a0
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 18:43:37 -0300

    Exercicio 2 Melhorado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
@@ -1,19 +1,19 @@
 // Exercicio 1
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
-}
+// for(let index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// }
 
 // Exercicio 2
 
-// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-// let resultado = 0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
 
-// for(index = 0; index < numbers.length; index += 1) {
-//   resultado += numbers[index]
-// }
+for(index = 0; index < numbers.length; index += 1) {
+  resultado += numbers[index]
+}
 
 // console.log(resultado);
 
