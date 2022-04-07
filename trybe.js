7682b2cbe64a80c6caf8194586e7117ccde807b1
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 11:54:00 -0300

    Exercicio 7 Completo)

--- trybe.js
+++ trybe.js
@@ -74,3 +74,20 @@ if(peca == "peão") {
   console.log("Peça Inválida")
 }
 
+const nota = 102;
+if(nota >= 90 && nota <= 100) {
+  console.log("A")
+} else if (nota >= 80 && nota < 90) {
+  console.log("B")
+} else if (nota >= 70 && nota < 80) {
+  console.log("C")
+} else if (nota >= 60 && nota < 70) {
+  console.log("D")
+} else if (nota >= 50 && nota < 60) {
+  console.log("E")
+} else if (nota < 50 && nota > 0) {
+  console.log("F")
+} else {
+  console.log("ERROR")
+}
+
