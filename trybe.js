8937beaf925f157f3447d7c405e20c0a0ed0a9d7
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 10:52:39 -0300

    Exercicio 2 Completo

--- trybe.js
+++ trybe.js
@@ -6,4 +6,13 @@ console.log(a - b)
 console.log(a * b)
 console.log(a / b)
 console.log(a % b)
-  
+
+const primeiroNumero = 90;
+const segundoNumero = 90;
+if(primeiroNumero > segundoNumero){
+  console.log(primeiroNumero)
+} else if (primeiroNumero < segundoNumero){
+  console.log(segundoNumero)
+} else {
+  console.log("São Iguais")
+}
