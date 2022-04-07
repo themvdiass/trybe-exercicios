574bf62f2a797b6e384456615053c5fa4ad71264
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 11:09:17 -0300

    Exercicio 3 Completo

--- trybe.js
+++ trybe.js
@@ -8,7 +8,7 @@ console.log(a / b)
 console.log(a % b)
 
 const primeiroNumero = 90;
-const segundoNumero = 90;
+const segundoNumero = 150;
 if(primeiroNumero > segundoNumero){
   console.log(primeiroNumero)
 } else if (primeiroNumero < segundoNumero){
@@ -16,3 +16,22 @@ if(primeiroNumero > segundoNumero){
 } else {
   console.log("São Iguais")
 }
+
+const numUm = 35;
+const numDois = 25;
+const numTres = 35;
+if(numUm > numDois && numUm > numTres){
+  console.log(numUm)
+} else if (numDois > numUm && numDois > numTres){
+  console.log(numDois)
+} else if (numTres > numUm && numTres > numDois) {
+  console.log(numTres)
+} else if (numUm == numDois & numUm > numTres) {
+  console.log(numUm + " " + "(Números 1 e 2 possuem o mesmo valor)")
+} else if (numUm == numTres & numUm > numDois) {
+  console.log(numUm + " " + "(Números 1 e 3 possuem o mesmo valor)")
+} else if (numDois == numTres & numDois > numUm) {
+  console.log(numDois + " " + "(Números 2 e 3 possuem o mesmo valor)")
+} else {console.log("São iguais")
+}
+
