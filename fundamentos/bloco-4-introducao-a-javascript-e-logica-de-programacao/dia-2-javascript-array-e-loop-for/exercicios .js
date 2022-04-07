94574cfe1b1380e00340a486a4ef5c67cd25f148
Vinicius Dias <themvdiass@gmail.com>
2022-04-07 18:42:59 -0300

    Exercicio 1 Melhorado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios .js
@@ -2,26 +2,37 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(numbers);
+for(let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
 
 // Exercicio 2
 
-console.log(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]);
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//   resultado += numbers[index]
+// }
+
+// console.log(resultado);
 
 // Exercicio 3
 
-let contagem = numbers.length;
-console.log((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem);
+// let contagem = numbers.length;
+// console.log((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem);
 
 // Exercicio 4
 
-let total = [];
-total.push((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem);
-if(total > 20) {
-  console.log("Valor maior que 20")
-} else {
-  console.log("Valor menor que 20")
-};
+// let total = [];
+// total.push((numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7]+ numbers[8] + numbers[9]) / contagem);
+// if(total > 20) {
+//   console.log("Valor maior que 20")
+// } else {
+//   console.log("Valor menor que 20")
+// };
+
+//Exercicio 5
 
 
 
