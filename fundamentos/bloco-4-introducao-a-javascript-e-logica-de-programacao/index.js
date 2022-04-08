e6c0f6804f6988146da1a346bc83f4367835b1ea
Vinicius Dias <themvdiass@gmail.com>
2022-04-08 16:04:04 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
@@ -10,11 +10,31 @@
 
 // Exercicio 2
 
-let word = 'tryber';
-let wordContrario = '';
+// let word = 'tryber';
+// let wordContrario = '';
 
-for(let index = 0; index < word.length; index += 1) {
-  wordContrario += word[word.length - 1 - index]
-};
+// for(let index = 0; index < word.length; index += 1) {
+//   wordContrario += word[word.length - 1 - index]
+// };
 
-console.log(wordContrario);
+// console.log(wordContrario);
+
+// Exercicio 3
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for(let index = 0; index < array.length; index += 1) {
+  if(array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index]
+  }
+  if(array[index].length < menorPalavra.length) {
+    menorPalavra = array[index]
+  }
+}
+
+console.log(maiorPalavra);
+
+console.log(menorPalavra);
+
