eb1dede2b9278572b37cecdde5271e385a1ccf63
Vinicius Dias <themvdiass@gmail.com>
2022-04-08 15:50:52 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
@@ -1,9 +1,20 @@
 // Exercicio 1
 
-let numero = 10;
-let fatorial = 1;
-for(let index = 1; index < 10; index += 1) {
-  fatorial = fatorial * index 
-}
+// let numero = 10;
+// let fatorial = 1;
+// for(let index = 1; index < 10; index += 1) {
+//   fatorial = fatorial * index 
+// }
 
-console.log(fatorial);
+// console.log(fatorial);
+
+// Exercicio 2
+
+let word = 'tryber';
+let wordContrario = '';
+
+for(let index = 0; index < word.length; index += 1) {
+  wordContrario += word[word.length - 1 - index]
+};
+
+console.log(wordContrario);
