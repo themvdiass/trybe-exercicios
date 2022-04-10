5161316a53f678047b6105cac341625c7a4a6e24
Vinicius Dias <themvdiass@gmail.com>
2022-04-10 18:42:20 -0300

    Exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/index.js
@@ -21,20 +21,38 @@
 
 // Exercicio 3
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maiorPalavra = array[0];
-let menorPalavra = array[0];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = array[0];
+// let menorPalavra = array[0];
 
-for(let index = 0; index < array.length; index += 1) {
-  if(array[index].length > maiorPalavra.length) {
-    maiorPalavra = array[index]
+// for(let index = 0; index < array.length; index += 1) {
+//   if(array[index].length > maiorPalavra.length) {
+//     maiorPalavra = array[index]
+//   }
+//   if(array[index].length < menorPalavra.length) {
+//     menorPalavra = array[index]
+//   }
+// }
+
+// console.log(maiorPalavra);
+
+// console.log(menorPalavra);
+
+
+// Exercicio 4
+
+let maiorNumero = 0;
+
+for (let primeiroNumero = 0; primeiroNumero <= 50; primeiroNumero += 1) {
+  let primo = true;
+  for (let divisor = 2; divisor < primeiroNumero; divisor += 1) {
+    if (primeiroNumero % divisor === 0) {
+      primo = false;
+    }
   }
-  if(array[index].length < menorPalavra.length) {
-    menorPalavra = array[index]
+  if(primo) {
+    maiorNumero = primeiroNumero;
   }
 }
 
-console.log(maiorPalavra);
-
-console.log(menorPalavra);
-
+console.log(maiorNumero);
