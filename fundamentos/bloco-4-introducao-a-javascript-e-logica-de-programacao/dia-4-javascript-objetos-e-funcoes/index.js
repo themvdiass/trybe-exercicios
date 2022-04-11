f5843f3ab2b7139075738870ed5c89cc46ef12f6
Vinicius Dias <themvdiass@gmail.com>
2022-04-11 13:17:52 -0300

    Teste 1.0

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
@@ -1,10 +1,10 @@
 // Exemplo 1
 
 let player = {
-  name = 'Marta',
-  lastName = 'Silva',
-  age = 34,
-  medals = { 
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { 
     golden: 2, 
     silver: 3 }
 }
