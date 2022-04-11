8c92c813b38f0a198ad2c8e6ef0f304f42c41d42
Vinicius Dias <themvdiass@gmail.com>
2022-04-11 15:11:13 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,7 +1,20 @@
+// Exercicio 1
+
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+
+// console.log('Bem-vinda' + ' ' + info.personagem);
+
+// Exercicio 2
+
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
 };
 
-console.log('Bem-vinda' + ' ' + info.personagem);
+console.log(info);
