917b031d83ec379bdad5dfab129436615b35cbe2
Vinicius Dias <themvdiass@gmail.com>
2022-04-11 15:46:57 -0300

    Exercicio 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -92,6 +92,26 @@
 
 //Exercicio 7
 
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//     {
+//       titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//       autor: 'JK Rowling',
+//       editor: 'Rocco',
+//     },
+//   ],
+// };
+
+// Exercicio 8
+
 let leitor = {
   nome: 'Julia',
   sobrenome: 'Pessoa',
@@ -109,3 +129,5 @@ let leitor = {
     },
   ],
 };
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.')
