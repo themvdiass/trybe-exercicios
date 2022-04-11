0d347c944d580c6c35f9422fc1f15395f05b1d99
Vinicius Dias <themvdiass@gmail.com>
2022-04-11 13:28:06 -0300

    Teste 1.3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
@@ -6,7 +6,16 @@ let player = {
   age: 34,
   medals: { 
     golden: 2, 
-    silver: 3 }
-}
+    silver: 3
+  },
+  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+};
 
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' ' + 'tem ' + player.age + ' ' + 'anos de idade.')
+let nomeCompleto = player.name + ' ' + player.lastName;
+
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' ' + 'tem ' + player.age + ' ' + 'anos de idade.');
+
+console.log('********************');
+
+console.log('A jogadora ' + nomeCompleto + ' ' + 'foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' ' + 'vezes.');
