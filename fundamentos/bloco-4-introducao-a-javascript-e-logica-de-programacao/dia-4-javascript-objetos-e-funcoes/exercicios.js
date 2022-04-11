9faf3e4860e48bd22e36ceadcf776db777747bda
Vinicius Dias <themvdiass@gmail.com>
2022-04-11 15:41:48 -0300

    Exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -47,30 +47,45 @@
 
 // Exercicio 5
 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim'
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// };
+
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O último MacPatinhas',
+//   recorrente: 'Sim'
+// };
+
+// for (let key in info) {
+//   if (
+//     key === 'recorrente' &&
+//     info[key] === 'Sim' &&
+//     info2[key] === 'Sim'
+//   ) {
+//     console.log('Ambos recorrentes');
+//   } else {
+//     console.log(info[key] + ' e ' + info2[key]);
+//   }
+// }
+
+// Exercicio 6
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim'
-};
-
-for (let key in info) {
-  if (
-    key === 'recorrente' &&
-    info[key] === 'Sim' &&
-    info2[key] === 'Sim'
-  ) {
-    console.log('Ambos recorrentes');
-  } else {
-    console.log(info[key] + ' e ' + info2[key]);
-  }
-}
-
-
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".')
