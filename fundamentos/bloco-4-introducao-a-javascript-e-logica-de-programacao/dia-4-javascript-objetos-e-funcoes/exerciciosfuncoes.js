f33aef4b2593c5c300ff59130ed83e2343afdbb3
Vinicius Dias <themvdiass@gmail.com>
2022-04-12 13:32:54 -0300

    Exercicio Function 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exerciciosfuncoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exerciciosfuncoes.js
@@ -1,13 +1,29 @@
 // Exercicio 1
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
-let palavra = 'arara';
+// let palavra = 'arara';
 
-function palindromo (palavra) {
-  for (index in palavra) {
-   if (palavra[index] != palavra[(palavra.length - 1) - index]) {
-    return false;
-    }
+// function palindromo (palavra) {
+  // for (index in palavra) {
+//    if (palavra[index] != palavra[(palavra.length - 1) - index]) {
+//     return false;
+//     }
+//   }
+//   return true;
+// }
+// console.log(palindromo('arara'))
+
+//Exercicio 2
+//Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function maiorIndice (array) {
+  let maiorNumero = 0;
+  for (index in array) {
+    if (array[maiorNumero] < array[index]) {
+      maiorNumero = index 
+    } 
   }
-  return true;
+  return maiorNumero
 }
-console.log(palindromo('arara'))
+
+console.log(maiorIndice([2, 3, 6, 7, 10, 1]))
