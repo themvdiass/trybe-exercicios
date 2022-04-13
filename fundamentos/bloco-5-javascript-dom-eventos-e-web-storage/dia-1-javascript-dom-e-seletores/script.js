35daa5401ed67f85cf295e050df5ff35eb833db1
Vinicius Dias <themvdiass@gmail.com>
2022-04-13 19:47:45 -0300

    Exercicio 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -2,3 +2,7 @@
 
 document.getElementById("meuTexto").innerText = "Me vejo trabalhando com programação, ganhando acima de 10k e viajando para onde quiser, quando quiser.";
 
+// 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+
+document.querySelector('#mainColor').style.backgroundColor = 'green';
+
