756b49d2413b9fceef05dab3f8a6865688d04b85
Vinicius Dias <themvdiass@gmail.com>
2022-04-13 19:47:08 -0300

    Exercicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,4 @@
+// 1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+document.getElementById("meuTexto").innerText = "Me vejo trabalhando com programação, ganhando acima de 10k e viajando para onde quiser, quando quiser.";
+
