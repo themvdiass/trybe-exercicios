bad92bdec9f2d85190e0e4f6c9c06009ca54b854
Vinicius Dias <themvdiass@gmail.com>
2022-04-18 11:59:08 -0300

    Exercicio 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/node.js
@@ -0,0 +1,10 @@
+// Exercicio 1
+
+const body = document.querySelector('body')
+
+const H1 = document.createElement('h1')
+H1.id = 'tagH1'
+H1.innerText = "Exercício 5.2 - JavaScript DOM"
+body.appendChild(H1)
+
+// Exercicio 2
