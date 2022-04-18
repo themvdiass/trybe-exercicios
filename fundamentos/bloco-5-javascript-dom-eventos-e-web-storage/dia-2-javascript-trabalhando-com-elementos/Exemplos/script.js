3de4830a39be97e6db84b4a0e1469f65c38982c6
Vinicius Dias <themvdiass@gmail.com>
2022-04-18 11:28:28 -0300

    Exemplos (Parte 2)

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/Exemplos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/Exemplos/script.js
@@ -1,9 +1,12 @@
 // Exemplo 1
 
-console.log(document.getElementById('elementoOndeVoceEsta'))
+const eOE = document.getElementById('elementoOndeVoceEsta')
+
+console.log(eOE)
 
 // Exemplo 2
-document.getElementById('elementoOndeVoceEsta').parentElement.style.color = "red"
+
+eOE.parentElement.style.color = "red"
 
 //  Exemplo 3
 
@@ -11,20 +14,54 @@ document.getElementById('primeiroFilhoDoFilho').innerText = "Novo Texto"
 
 //  Exemplo 4
 
-console.log(document.getElementById('pai').firstElementChild)
+const eP = document.getElementById('pai')
+
+console.log(eP.firstElementChild)
 
 //  Exemplo 5
 
-console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling)
+console.log(eOE.previousElementSibling)
 
 // Exemplo 6
 
-console.log(document.getElementById('elementoOndeVoceEsta').nextSibling)
+console.log(eOE.nextSibling)
 
 //  Exemplo 7 
 
-console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling)
+console.log(eOE.nextElementSibling)
 
 // Exemplo 8
 
-console.log(document.getElementById('pai').lastElementChild.previousElementSibling)
+console.log(eP.lastElementChild.previousElementSibling)
+
+console.log(' ')
+
+// Parte 2
+
+// Exemplo 1
+
+let div1 = document.createElement('div')
+div1.id = "div1"
+div1.innerText = "Novo Texto"
+eP.appendChild(div1)
+
+// Exemplo 2
+
+let div2 = document.createElement('div') 
+div2.id = "div2"
+eOE.appendChild(div2)
+
+// Exemplo 3
+
+let div3 = document.createElement('div')
+div3.id = "div3"
+document.getElementById('primeiroFilhoDoFilho').appendChild(div3)
+
+// Exemplo 4
+
+console.log(div3.parentElement.parentElement.nextElementSibling)
+
+
+
+
+
