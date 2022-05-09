f2ad72110216695818b6fb94ff5bfb64fdbbe7dd
Vinicius Dias <themvdiass@gmail.com>
2022-05-09 15:44:44 -0300

    Pequeno Exercicios do dia

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -1,13 +1,13 @@
 // Teste de Arrow Function
 
 const sum = (value1, value2) => value1 + value2;
-console.log(sum(2, 5));
+console.log(sum(2, 3));
 
 
 // Teste de Arrow Function (com valores de tipos diferentes)
 
 const sum2 = (value1, value2) => value1 + value2;
-console.log(sum2(2, "5"));
+console.log(sum2(2, "3"));
 
 // Teste de Condicional
 
@@ -19,3 +19,33 @@ const sum3 = (value1, value2) => {
 };
 
 console.log(sum3(2, '3'));
+
+// Teste de Condicional com Throw
+
+const sum4 = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+  return value1 + value2;
+};
+
+console.log(sum4(2, '3'));
+
+// Teste de Condicional com uso de catch
+
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum5 = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+console.log(sum5(2, '3'));
