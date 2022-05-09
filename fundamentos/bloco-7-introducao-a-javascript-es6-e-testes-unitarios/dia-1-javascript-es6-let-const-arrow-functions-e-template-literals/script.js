3d2bce3c18bcb613738fbb2483b23b71fc704570
Vinicius Dias <themvdiass@gmail.com>
2022-05-09 15:32:21 -0300

    Teste arrow function

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,10 @@
+// Teste de Arrow Function
+
+const sum = (value1, value2) => value1 + value2;
+console.log(sum(2, 5));
+
+
+// Teste de Arrow Function (com valores de tipos diferentes)
+
+const sum2 = (value1, value2) => value1 + value2;
+console.log(sum2(2, "5"));
