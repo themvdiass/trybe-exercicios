8c30babb97552533047d3ec72ad670dac6d232b1
Vinicius Dias <themvdiass@gmail.com>
2022-05-10 16:04:24 -0300

    Teste com toEqual

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
@@ -14,7 +14,18 @@ test('sums two values', () => {
   expect(sum(2, 3)).toEqual(5);
 });
 
-// Exercicio de jest
+// Exercicio de jest (toBe)
 
 expect(5).toBe("5")
 
+// Exercicio de jest (toEqual)
+
+test('array and object equality', () => {
+  const arr = [1, 2 ,3];
+  const obj = { a: 1, b: 2, c: 3};
+
+  expect(arr).toBe([1, 2, 3]); // fails
+  expect(obj).toBe({ a: 1, b: 2, c: 3}); // fails
+  expect(arr).toEqual([1, 2, 3]); // OK
+  expect(obj).toEqual({ a: 1, b: 2, c: 3}); // OK
+});
