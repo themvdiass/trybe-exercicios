18b754668d1a44d0f09707a50bb300f7c226771f
Vinicius Dias <themvdiass@gmail.com>
2022-05-13 15:20:22 -0300

    Exercicio fixação

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -1,4 +1,4 @@
-// Exercicio 1
+// // Exercicio 1
 
 const sayHello = () => {
   return ('hello trybers');
@@ -13,3 +13,28 @@ printGreeting(sayHello);
 
 // console.log(sayHello());
 
+const sumFixAmount = (amount) => {
+  return (number) => amount + number;
+}
+
+const initialSum = sumFixAmount(15)
+console.log(initialSum(5));
+
+
+// Exercicio fixação
+
+const wakeUp = () => {
+  return 'Acordando!!';
+}
+
+const comendo = () => {
+  return 'Bora tomar café!!';
+}
+
+const dormir = () => {
+  return 'Partiu dormir!!';
+}
+
+const doingThings = (func) => console.log(func());
+
+doingThings(wakeUp);
