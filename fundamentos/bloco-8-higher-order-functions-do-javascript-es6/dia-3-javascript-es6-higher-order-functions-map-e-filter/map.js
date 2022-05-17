e7679a06bc70685d0963b5f7b1afa5893bb5aa1d
Vinicius Dias <themvdiass@gmail.com>
2022-05-17 15:29:47 -0300

    Exercises map

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/map.js
@@ -0,0 +1,54 @@
+// EXERCISE 1
+// (usando for)
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+// (usando map)
+
+const persons2 = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames2 = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames2); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+// EXERCISE 2
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+
+// EXERCISE 3
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
