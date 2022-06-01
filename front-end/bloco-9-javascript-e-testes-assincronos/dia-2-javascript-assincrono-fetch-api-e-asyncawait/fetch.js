1eac45a983d73e9b4faecc33599e382608fe2cbd
Vinicius Dias <themvdiass@gmail.com>
2022-06-01 16:01:34 -0300

    Exemplos

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fetch.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+// JEITO I
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.value)
+    .catch((error) => `Algo deu errado :( \n${error}`);
+
+  console.log(result);
+};
+
+fetchJoke();
+
+// JEITO II
+const fetchJoke2 = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+
+fetchJoke2();
