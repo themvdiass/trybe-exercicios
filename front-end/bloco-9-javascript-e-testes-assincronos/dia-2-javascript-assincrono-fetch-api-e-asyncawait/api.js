436d268d32f1ecaa71011e8864fb050ddeea24f0
Vinicius Dias <themvdiass@gmail.com>
2022-06-02 11:53:29 -0300

    Exercise 3 (Sem HoF)

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -0,0 +1,39 @@
+const API_URL = "https://api.coincap.io/v2/assets";
+
+function criarLi(info) {
+ 
+
+  for (let index = 0; index < info.length; index += 1) {
+
+    const ul = document.querySelector('ul');
+
+    const li = document.createElement('li');
+
+    const divNome = document.createElement('div');
+
+    divNome.innerHTML = `${info[index].name} (${info[index].symbol}): ${info[index].priceUsd}`
+  
+  li.appendChild(divNome)
+
+  ul.appendChild(li)
+  };
+}
+
+async function fetchCoin() {
+  try {
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+
+    const dataAtt = data.data;
+
+    console.log(dataAtt);
+    
+    criarLi(dataAtt);
+
+  } catch (error) {
+    console.log('Vish deu erro')
+  }
+}
+
+window.onload = fetchCoin;
