0b153b28bd06d0738d5602a33084dfa239736f51
Vinicius Dias <themvdiass@gmail.com>
2022-06-20 15:16:03 -0300

    Exercicios props

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/examples/fixation-exercises-10-2/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Image from './Image';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" /> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
