b24f9f2c80be4655ca2f07cf26891b17de8fb96b
Vinicius Dias <themvdiass@gmail.com>
2022-06-20 15:31:27 -0300

    Exercicios components

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/examples/fixation-exercises-10-2/src/App.jsx
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/examples/fixation-exercises-10-2/src/App.jsx
@@ -1,14 +1,44 @@
 import './App.css';
-import Image from './Image';
+import Album from './Album';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" /> 
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const album01 = {
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Coldplay_-_Mylo_Xyloto_%28Official_Album_Cover%29.png/220px-Coldplay_-_Mylo_Xyloto_%28Official_Album_Cover%29.png',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    const album02 = {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    return (
+      <div>
+        <Album album={ album01 } />
+        <Album album={ album02 } />
+      </div>
+    );
+  }
 }
 
 export default App;
